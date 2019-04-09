a9c62c68724dc855fd1f13da4880b2d302590947
Triumf <triumf_96@hotmail.com>
2019-04-09 23:09:07 +0200

    new style of campaigns

--- resources/app/src/main.js
+++ resources/app/src/main.js
@@ -5,7 +5,17 @@ import {store} from '@/store'
 import '@/assets/scss/main.scss'
 
 import libraries from './libraries'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import VueCkeditor from 'vue-ckeditor5'
 
+const options = {
+  editors: {
+    classic: ClassicEditor,
+  },
+  name: 'ckeditor'
+}
+
+Vue.use(VueCkeditor.plugin, options);
 Vue.use(libraries)
 Vue.config.productionTip = false
 
